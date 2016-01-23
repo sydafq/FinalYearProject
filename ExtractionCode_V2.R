b04ff9a1890e939116a31d412bab802d4030e1df
Afiq <syd.afiq@hotmail.com>
2016-01-23 21:14:14 +0800

    Created another version of the Script using the initial prototype. Version 2 utlizes database incase of stream gets cut off

---
+++ ExtractionCode_V2.R
@@ -0,0 +1,28 @@
+#More Simplistic version running on RSQLite
+#Initialise Libraries
+library(DBI)
+library(RMySQL)
+library(sqldf)
+library("twitteR")
+library("tm")
+
+#necessary file for Windows
+download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem")
+apitoken <- "4123804573-GJqGKJDXEalfrREL4MjVIwyXLyFVX31e6cC4DbU"
+apisecret <- "FoQx4uYEXiXMukOhwA6zapDAT5s9IXJFj3LfjDAZMQMWe"
+conskey <- "RHJkBog0391gr89wSB2wQ1v7l"
+conssecret <- "2RLqMNtTAQmNhOBsLBvG61WmvAuitXZSXUZOmCWoNePQvgeNjR"
+origop <- options("httr_oauth_cache")
+options(httr_oauth_cache=TRUE)
+
+#connects to the app
+setup_twitter_oauth(conskey,conssecret,apitoken,apisecret)
+
+#sets the cache (usually manually prompted but now automated)
+options(httr_oauth_cache=origop)
+
+#registers the backend database of where the tweets are redirected to
+register_sqlite_backend("StreamData")
+
+#starts query and searches
+search_twitter_and_store("#Trump",table_name = "tweets")
